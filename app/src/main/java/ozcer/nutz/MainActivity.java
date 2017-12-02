39d457d39d635f973b3595ca1b5890281f0a0c09
ozcer <iamreallyoscar@hotmail.com>
2017-12-02 11:40:26 -0500

    can move to search act

--- app/src/main/java/ozcer/nutz/MainActivity.java
+++ app/src/main/java/ozcer/nutz/MainActivity.java
@@ -1,7 +1,10 @@
 package ozcer.nutz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
@@ -14,5 +17,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button searchBtn = (Button) findViewById(R.id.searchCourseBtn);
+
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SearchCourse.class);
+                startActivity(intent);
+            }
+        });
     }
 }
