8fe98ed961d0c11af52ae9e3f4e35cd7898545c5
ozcer <iamreallyoscar@hotmail.com>
2017-12-02 11:13:12 -0500

    added basic interface to main

--- app/src/main/java/ozcer/nutz/MainActivity.java
+++ app/src/main/java/ozcer/nutz/MainActivity.java
@@ -3,7 +3,12 @@ package ozcer.nutz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    String apiKey = "aCmmLsCQbeovDkMfOtcUbzkLxcYvChMm";
+    ArrayList<String> searchResult = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
