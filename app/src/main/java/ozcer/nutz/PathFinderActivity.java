8fe59235218793218ea84655692323cf9efeaa54
ozcer <iamreallyoscar@hotmail.com>
2017-12-02 18:18:03 -0500

    pushing garbage

--- app/src/main/java/ozcer/nutz/PathFinderActivity.java
+++ app/src/main/java/ozcer/nutz/PathFinderActivity.java
@@ -3,9 +3,16 @@ package ozcer.nutz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
+import ozcer.nutz.Structs.Course;
+
+import java.util.ArrayList;
+
+import ozcer.nutz.StoreLocal.UserInfo;
+import ozcer.nutz.StoreLocal.UserInfoSingleton;
 import ozcer.nutz.Structs.Course;
 
 public class PathFinderActivity extends AppCompatActivity {
@@ -36,4 +43,43 @@ public class PathFinderActivity extends AppCompatActivity {
         }
         return leaves;
     }
+    /*
+    public void pathFinding(Course goal, Integer startTerm) {
+        int[] terms = {1,5,9};
+        ArrayList<ArrayList> semesters = new ArrayList<>();
+        Integer thisTerm = startTerm;
+
+        UserInfo user = UserInfoSingleton.getInstance();
+        ArrayList<Course> futureTaken = new ArrayList<>(user.takenCourses);
+
+        String goalName = goal.getCourseCode();
+
+        // all the courses
+        ArrayList<Course> leafs = new ArrayList<>();
+
+        // loop until goal is the only leaf left and it's available this term
+        while (!(leafs.size() != 1 && leafs.get(0).getCourseCode().equals(goalName) && goal.availableTerms.contains(thisTerm))) {
+            ArrayList<Course> termLoad = new ArrayList<>(); // new semester
+            // for every course
+            for(Course leaf: leafs) {
+                // if available this term
+                if(leaf.availableTerms.contains(thisTerm)) {
+                    // add to current termLoad
+                    termLoad.add(leaf);
+                    futureTaken.add(leaf);
+                    // if all parent prereqs are in future taken, pop self add parent
+                    if()
+                }
+                // add semester planning to semesters
+                // go to next term
+            }
+        }
+        // if all reqs are filled
+        ArrayList<Course> theTermYouReachGoal = new ArrayList<>();
+        theTermYouReachGoal.add(goal);
+        semesters.add(theTermYouReachGoal);
+
+
+    }
+    */
 }
