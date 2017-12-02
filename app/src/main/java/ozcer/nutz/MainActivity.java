e7bb9a335d72c4ad86a97d493fa6a3c2d9048479
Jiayi Zhao <jeff.no.zhao@gmail.com>
2017-12-02 14:50:14 -0500

    fix master branch

--- app/src/main/java/ozcer/nutz/MainActivity.java
+++ app/src/main/java/ozcer/nutz/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
     userBtn.setOnClickListener(new View.OnClickListener() {
     @Override
     public void onClick(View view) {
-      Intent intent = new Intent(MainActivity.this, UserInfo.class);
+      Intent intent = new Intent(MainActivity.this, UserActivity.class);
       startActivity(intent);
     }
     });
