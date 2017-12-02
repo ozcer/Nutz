36f31db5e0176acfb4cc691dda68a348ab09fc18
Jiayi Zhao <jeff.no.zhao@gmail.com>
2017-12-02 14:56:18 -0500

    add UserActivity.java to Manifest

--- app/src/main/java/ozcer/nutz/MainActivity.java
+++ app/src/main/java/ozcer/nutz/MainActivity.java
@@ -7,10 +7,6 @@ import android.view.View;
 import android.widget.Button;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import ozcer.nutz.Structs.Course;
-import ozcer.nutz.Structs.CourseBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
