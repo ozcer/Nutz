f90b7da06fb2b5be1e00fc4f02fb202ba6511f77
Jiayi Zhao <jeff.no.zhao@gmail.com>
2017-12-02 17:11:07 -0500

    try to load and save user info add method to get courses with no prereqs

--- app/src/main/java/ozcer/nutz/PathFinderActivity.java
+++ app/src/main/java/ozcer/nutz/PathFinderActivity.java
@@ -3,11 +3,28 @@ package ozcer.nutz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ozcer.nutz.Structs.Course;
+
 public class PathFinderActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_path_finder);
+    }
+
+    protected ArrayList<Course> getLeafCourses(Course rootCourse) {
+        ArrayList<Course> leaves = new ArrayList<>();
+        leaves.add(rootCourse);
+        for (Course course : leaves) {
+            List<Course> prereqCourses = course.getPrereqs();
+            if (prereqCourses != null && !prereqCourses.isEmpty()) {
+                leaves.addAll(prereqCourses);
+            }
+        }
+        return leaves;
     }
 }
